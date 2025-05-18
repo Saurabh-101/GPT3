7e9b42f46320c5b1754e31633122ff072f7a742d
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-05-19 02:10:02 +0530

    Navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import React, { useState } from 'react'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 import './navbar.css'
-const navbar = () => {
+const Menu = () =>(
+  <>
+  <p><a href="#Home">Home</a></p>
+  <p><a href="#wgpt3">What is GPT3</a></p>
+  <p><a href="#possibility">Open AI</a></p>
+  <p><a href="#features">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
+  </>
+)
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <>
       <div className='gpt3__navbar'>
@@ -10,10 +20,37 @@ const navbar = () => {
           <div className="gpt3__navbar-links_logo">
             <img src={logo} alt="Logo" />
           </div>
+          <div className='gpt3__navbar-links_container'>
+            <Menu/>
+          </div>
+          
+        </div>
+        <div className='gpt3__navbar-sign'>
+          <p>Sign in</p>
+          <button>Sign Up</button>
+        </div>
+        <div className='gpt3__navbar-menu'>
+            {toggleMenu 
+              ? <RiCloseLine color='#ffff' size={27} onClick={()=>{setToggleMenu(false)}}/> : <RiMenu3Line color='#ffff' size={27} onClick={()=>{setToggleMenu(true)}}/> 
+            }
+            {
+              toggleMenu && 
+              (
+                <div className='gpt3__navbar-menu_container scale-up-center'>
+                  <div className='gpt3__navbar-menu_container-links'>
+                    <Menu />
+                    <div className='gpt3__navbar-menu_container-links-sign'>
+                    <p>Sign in</p>
+                    <button>Sign Up</button>
+                    </div>
+                  </div>
+                </div>
+              )
+            }
         </div>
       </div>
     </>
   )
 }
 
-export default navbar
+export default Navbar
