0a2933c83d753c3db9228c3c2251c358509884a8
Saurabh-101 <saurabhgupta72dh3@gmail.com>
2025-05-20 14:51:59 +0530

    WhatGPT3 component is done and working with feature subComponent in it

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
-const feature = () => {
+import './feature.css';
+const Feature = ({title,text}) => {
   return (
-    <div>feature</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+        <div></div>
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container__feature-text'>
+        {text}
+      </div>
+    </div>
   )
 }
 
-export default feature
+export default Feature
